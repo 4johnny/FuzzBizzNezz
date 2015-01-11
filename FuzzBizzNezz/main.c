8f22350a120599e3497143eca869c6baca93df11
4johnny <4johnny@users.noreply.github.com>
2015-01-11 02:07:45 -0800

    Initial Commit

---
+++ FuzzBizzNezz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FuzzBizzNezz
+//
+//  Created by Johnny Hermann on 2015-01-11.
+//  Copyright (c) 2015 Johnny Hermann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
